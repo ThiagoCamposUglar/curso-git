340cbef6481e86b70daefb1f2d7d6c7d2b165083
Thiago Campos <thiagocampos2002@gmail.com>
2021-07-13 10:50:17 -0300

    Pede o numero de linhas e colunas, depois coloca as linhas e colunas, e mostra a diagonal da matriz e os numeros negativos

---
+++ Produtinho/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produtinho {
+    class Produto {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto() {//default constructor method
+
+        }
+
+        public Produto(string nome, double preco) : this() {//constructor using this to get stuff from the default constructor
+            _nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
+            Quantidade = quantidade;
+        }
+
+
+        //using get set method to get and set values from the encapsulated stuff(the priavte variables)
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque() {
+            double total = Quantidade * Preco;
+            return total;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return _nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Produtinho/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Produtinho {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            //Produto p1 = new Produto(nome, preco);
+
+            //Produto p2 = new Produto();
+
+            /*Produto p3 = new Produto {
+                Nome = nome,
+                Preco = preco,
+                Quantidade = quantidade
+             };*/
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos a serem adicionados ao estoque:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos a serem removidos do estoque:");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
